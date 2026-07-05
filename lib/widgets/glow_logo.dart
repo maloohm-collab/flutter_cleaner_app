eafc0554bb3e2d7ad26ca2b6b8605977e5c0b980
maloohm-collab <maloohm@gmail.com>
2026-07-05 12:39:36 +0300

    Add GlowLogo widget with animation and gradient

--- lib/widgets/glow_logo.dart
+++ lib/widgets/glow_logo.dart
@@ -1,1 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
+import '../utils/colors.dart';
+
+class GlowLogo extends StatelessWidget {
+  final double size;
+  final IconData icon;
+
+  const GlowLogo({
+    super.key,
+    this.size = 120,
+    this.icon = Icons.rocket_launch_rounded,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        gradient: AppColors.primaryGradient,
+        boxShadow: [
+          BoxShadow(
+            color: AppColors.primary.withOpacity(0.45),
+            blurRadius: 35,
+            spreadRadius: 8,
+          ),
+        ],
+      ),
+      child: Icon(
+        icon,
+        size: size * 0.5,
+        color: Colors.white,
+      ),
+    )
+        .animate(onPlay: (controller) => controller.repeat(reverse: true))
+        .scale(
+          begin: const Offset(0.95, 0.95),
+          end: const Offset(1.05, 1.05),
+          duration: 1800.ms,
+          curve: Curves.easeInOut,
+        );
+  }
+}
