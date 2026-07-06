52ba32a1a72313ad558e822f5a93d96d8ad91d17
maloohm-collab <maloohm@gmail.com>
2026-07-06 11:35:17 +0300

    Add ScanResultCard widget for scan item display
    Implement ScanResultCard widget to display scan item details with a checkbox.

---
+++ lib/widgets/scan_result_card.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import '../services/models/scan_item.dart';
+import '../utils/colors.dart';
+
+class ScanResultCard extends StatelessWidget {
+  final ScanItem item;
+  final ValueChanged<bool?> onChanged;
+
+  const ScanResultCard({
+    super.key,
+    required this.item,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 14),
+      decoration: BoxDecoration(
+        color: AppColors.card,
+        borderRadius: BorderRadius.circular(18),
+      ),
+      child: CheckboxListTile(
+        value: item.selected,
+        onChanged: onChanged,
+        activeColor: AppColors.primary,
+        secondary: const Icon(
+          Icons.auto_delete,
+          color: AppColors.primary,
+        ),
+        title: Text(
+          item.title,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        subtitle: Text(
+          "${item.files} files\n${item.readableSize}",
+        ),
+      ),
+    );
+  }
+}
